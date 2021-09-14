864cdd5f98a9f3d8660525e1cb7964220dacc144
danvig <62083658+danvig@users.noreply.github.com>
2021-09-14 15:16:07 +1000

    Some initial setup code

---
+++ src/MessageDecoder.java
@@ -0,0 +1,33 @@
+/*
+ 	JAVA MESSAGE DECODER
+ 	VER: 1.0
+ 	DATE: 14/9/2021
+ 	AUTHOR: DANIEL VIGLIETTI
+ */
+
+// Import dependencies and frameworks
+import java.util.*;
+
+// Main class
+public class MessageDecoder {
+
+	public static void main(String args[]) {
+		// SETUP SCANNER
+		Scanner sc = new Scanner(System.in);
+		
+		// INITIALISE
+		System.out.println("MESSAGE ENCODER");
+		System.out.println("Initialising...");
+		
+		// GET MESSAGE
+		System.out.println("\nINPUT MESSAGE");
+		String message = sc.nextLine();
+		
+		// GET SHIFT VALUE
+		System.out.println("\nENTER SHIFT-BY NUMBER");
+		int shift = sc.nextInt();
+		
+		// OPERATION
+		
+	}
+}
